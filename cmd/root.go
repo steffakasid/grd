1f06bc39d39ff563e331c466e7882cc6f18f7bc0
steffakasid <github@steffen-rumpf.de>
2020-12-12 22:41:23 +0100

    Initial

--- cmd/root.go
+++ cmd/root.go
@@ -17,35 +17,57 @@ limitations under the License.
 */
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/spf13/cobra"
 	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/xanzy/go-gitlab"
 
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
 )
 
 var (
-	cfgFile, newName string
-	deleteOldDefault bool
+	cfgFile       string
+	gitLabAdapter *gitlab.Client
 )
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "grd {gitlab_group_ID} [--flags]",
+	Args:  cobra.ExactArgs(1),
 	Short: "grd renames the default branch of all projects in a group",
 	Long: `Grd will rename the default branch of all projects within a group.
 	Therefore all projects are looked up. If the default branch already matches,
 	the new-name (defaults to 'main') it does nothing. Otherwise it will create a
 	new branch based on the old default branch. Sets the new branch to default and
 	protects it. Afterwards the old default is unprotected. Optionally you can also
-	delete the old default.
-	
+  delete the old default.
+  
+  Each flag could also be set as env var prefixed with GRD_ e.g. to set the token you 
+  can do 'export GRD_TOKEN=<your token>'.
+
 	Examples:
-	grd 1234    			- Rename all default branches to 'main' of group 1234 and keep the old default
+	grd 1234    			      - Rename all default branches to 'main' of group 1234 and keep the old default
 	grd 1234 --new-name 1st - Rename all default branches to '1st' of group 1234 and keep the old default
-	grd 1234 --delete		- Rename all default branches to 'main' of group 1234 and delete the old default`,
-	Run: func(cmd *cobra.Command, args []string) { },
+  grd 1234 --delete		    - Rename all default branches to 'main' of group 1234 and delete the old default
+  grd 1234 --unprotect    - Rename all default branches to 'main' of group 1234 and unprotect the old default`,
+	Run: func(cmd *cobra.Command, args []string) {
+		var err error
+		result, _ := json.Marshal(viper.AllSettings())
+		fmt.Println(string(result))
+		gitLabAdapter, err = gitlab.NewClient(viper.GetString("token"), gitlab.WithBaseURL(viper.GetString("url")))
+		doWePanic(err)
+
+		group, _, err := gitLabAdapter.Groups.GetGroup(args[0])
+		doWePanic(err)
+
+		for _, project := range group.Projects {
+			renameDefault(project)
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -60,34 +82,114 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
+	viper.SetDefault("new-name", "main")
+	viper.SetDefault("url", "https://gitlab.com")
+	viper.SetDefault("unprotect", false)
+	viper.SetDefault("delete", false)
+	viper.SetDefault("devs-can-merge", false)
+	viper.SetDefault("devs-can-push", false)
+
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.grd.yaml)")
-	rootCmd.Flags().StringVarP(&newName, "new-name", "n", "main", "Set the newname to rename the default to")
-	rootCmd.Flags().BoolVarP(&deleteOldDefault, "delete", "d", false, "Delete the old default branch when done")
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().StringP("new-name", "n", viper.GetString("new-name"), "Set the newname to rename the default to")
+	rootCmd.Flags().StringP("token", "t", "", "GitLab Token (API access) to be used")
+	rootCmd.Flags().StringP("url", "u", viper.GetString("url"), "GitLab API URL")
+	rootCmd.Flags().BoolP("unprotect", "p", viper.GetBool("unprotect"), "Unprotect the old default branch when done")
+	rootCmd.Flags().BoolP("delete", "d", viper.GetBool("delete"), "Delete the old default branch when done")
+	rootCmd.Flags().Bool("devs-can-merge", viper.GetBool("devs-can-merge"), "For new protected branch: Are developers allowed to merge?")
+	rootCmd.Flags().Bool("devs-can-push", viper.GetBool("devs-can-push"), "For new protected branch: Are developers allowed to push?")
+
+	viper.BindPFlag("new-name", rootCmd.Flags().Lookup("new-name"))
+	viper.BindPFlag("token", rootCmd.Flags().Lookup("token"))
+	viper.BindPFlag("delete", rootCmd.Flags().Lookup("delete"))
+	viper.BindPFlag("unprotect", rootCmd.Flags().Lookup("unprotect"))
+	viper.BindPFlag("devs-can-merge", rootCmd.Flags().Lookup("devs-can-merge"))
+	viper.BindPFlag("devs-can-push", rootCmd.Flags().Lookup("devs-can-push"))
 }
 
-// initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	if cfgFile != "" {
 		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
-		// Find home directory.
 		home, err := homedir.Dir()
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
-		// Search config in home directory with name ".grd" (without extension).
 		viper.AddConfigPath(home)
 		viper.SetConfigName(".grd")
 	}
-
-	viper.AutomaticEnv() // read in environment variables that match
+	viper.SetEnvPrefix("GRD")
+	viper.AutomaticEnv()
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
+}
+
+func doWePanic(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func renameDefault(proj *gitlab.Project) {
+	defaultBranchName := proj.DefaultBranch
+	allBranches, _, err := gitLabAdapter.Branches.ListBranches(proj.ID, &gitlab.ListBranchesOptions{})
+	doWePanic(err)
+	if len(allBranches) > 0 {
+		if defaultBranchName == viper.GetString("new-name") {
+			fmt.Printf("[%s] Nothing todo, %s is already the default branch!\n", proj.Name, viper.GetString("new-name"))
+		} else {
+			fmt.Printf("[%s] Current defaultBranch is %s\n", proj.Name, defaultBranchName)
+
+			if !branchAlreadyExists(viper.GetString("new-name"), allBranches) {
+				newBranch, _, err := gitLabAdapter.Branches.CreateBranch(proj.ID, &gitlab.CreateBranchOptions{
+					Branch: gitlab.String(viper.GetString("new-name")),
+					Ref:    gitlab.String(defaultBranchName),
+				})
+				doWePanic(err)
+				fmt.Printf("[%s] Created new branch%s\n", proj.Name, newBranch.Name)
+			}
+
+			_, _, err = gitLabAdapter.Projects.EditProject(proj.ID, &gitlab.EditProjectOptions{
+				DefaultBranch: gitlab.String(viper.GetString("new-name")),
+			})
+			doWePanic(err)
+			fmt.Printf("[%s] Set default branch to %s\n", proj.Name, viper.GetString("new-name"))
+
+			_, _, err = gitLabAdapter.Branches.ProtectBranch(proj.ID, viper.GetString("new-name"), &gitlab.ProtectBranchOptions{
+				DevelopersCanPush:  gitlab.Bool(viper.GetBool("devs-can-push")),
+				DevelopersCanMerge: gitlab.Bool(viper.GetBool("devs-can-merge")),
+			})
+			doWePanic(err)
+			fmt.Printf("[%s] Protected branch %s\n", proj.Name, viper.GetString("new-name"))
+
+			if viper.GetBool("unprotect") {
+				branch, _, err := gitLabAdapter.Branches.UnprotectBranch(proj.ID, *gitlab.String(defaultBranchName))
+				doWePanic(err)
+				fmt.Printf("[%s] Unprotected branch %s\n", proj.Name, branch.Name)
+			}
+
+			if viper.GetBool("delete") {
+				_, err := gitLabAdapter.Branches.DeleteBranch(proj.ID, *gitlab.String(defaultBranchName))
+				doWePanic(err)
+				fmt.Printf("[%s] Deleted branch %s\n", proj.Name, defaultBranchName)
+			}
+		}
+	} else {
+		fmt.Printf("[%s] Doesn't contain any branches! Nothing to do.\n", proj.Name)
+	}
+	fmt.Println("----------------------------------------------------")
+}
+
+func branchAlreadyExists(branchName string, branches []*gitlab.Branch) bool {
+	for _, branch := range branches {
+		if branchName == branch.Name {
+			return true
+		}
+	}
+	return false
 }
